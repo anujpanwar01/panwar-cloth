c374ee1134051d0694e345d62ef464631dd4d9fe
anujpanwar001 <anujpanwar2515@gmail.com>
2022-03-26 11:59:53 +0530

    adding cart context and shopping icon as well as cart-drop-down

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Outlet, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 // import SignIn from "./routes/sign-in/sign-in.comopnent";
-const Shop = function () {
-  return (
-    <div>
-      <div>
-        <h1>shop</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+// const Shop = function () {
+//   return (
+//     <div>
+//       <div>
+//         <h1>shop</h1>
+//       </div>
+//       <Outlet />
+//     </div>
+//   );
+// };
 
 const App = function () {
   return (
